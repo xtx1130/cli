7ffe247e8b847c0b2d29509c8b1dfbfb3465b796
xtx1130 <xtx1130@gmail.com>
2016-11-07 17:00:39 +0800

    fix bug for sh and add nginx controller

--- switch.sh
+++ switch.sh
@@ -1,39 +1,37 @@
-#!/bin/bash
-#切换本机hosts环境
+#!/bin/zsh
 file="/etc/hosts";
+source ~/.zshrc;
 type=0;
 env[1]="开发" env[2]="测试"  env[3]="线上";
-
-echo "现在是 ${env[$HOSTS]} 环境";
+echo "现在是${env[$HOSTS]}环境";
 read -p "选择切换到：1.开发  2.测试  3.线上 : " type;
-
-if [ $HOSTS -eq $type ];
-then
-        echo "环境不变"
-        return
-fi
+#if [[ $HOSTS -eq $type ]];
+#then
+#        echo "环境不变"
+#        return
+#fi
 case $type in
 1)
-        echo "正切换到开发环境。。"
-                sed -i '/#dev/,/#end/s/^ / /' $file;
-                sed -i '/#online/,/#dev/s/# / /' $file;
+        echo "正切换到开发环境"
+                sed -i "" '/testcdn/,/end/s/^/#/' $file;
+                sed -i "" '/localcdn/,/end/s/#//g' $file;
+                nginx;
         ;;
 2)
         echo "正切换到测试环境"
-                sed -i '/#dev/,/#end/s/^ /# /' $file;
-                sed -i '/#online/,/#dev/s/# / /' $file;
+                sed -i "" '/testcdn/,/end/s/^/#/' $file;
+                sed -i "" '/testcdn/,/localcdn/s/#//g' $file;
+                nginx -s stop;
         ;;
 3)
         echo "正切换到线上"
-                sed -i '/#test/,/#end/s/^ /# /' $file;
-                sed -i '/#online/,/#test/s/ / /' $file;
+                sed -i "" '/testcdn/,/end/s/^/#/' $file;
         ;;
 *)
         echo "输入错误"
         return
         ;;
 esac
-sed -i "s/HOSTS=$HOSTS/HOSTS=$type/" ~/.bash_profile;
-source ~/.bash_profile;
+sed -i "" "s/HOSTS=$HOSTS/HOSTS=$type/" ~/.zshrc;
 cat $file;
 killall -HUP mDNSResponder;
