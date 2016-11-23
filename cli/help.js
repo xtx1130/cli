0d084a940ac7ab281855430a652e81b23d9d35fa
xtx1130 <xtx1130@gmail.com>
2016-11-23 10:43:08 +0800

    update help

--- cli/help.js
+++ cli/help.js
@@ -1,19 +1,30 @@
-var f = function(){
-	this.description='help commend'
-	this.action=function(){
-		return function(){
-    		console.log('babel          ---------es6to5')
-    		console.log('folder          ---------establish folder')
-    		console.log('help     -h     ---------LJ info')
-    		console.log('cinit          ---------commonjs browserify')
-    		console.log('uglify          ---------compass javascript')
-            console.log('ci          ---------auto online update')
-            console.log('server          ---------establish localserver')
-            console.log('cdn          ---------auto online update')
-            console.log('     -l          local host')
-            console.log('     -t          test(dev) host')
-            console.log('     -o          online host')
+var f = function() {
+    this.description = 'help commend'
+    this.action = function() {
+        return function() {
+            process.stdout.write('\n' + [
+                'Usage: leju [Command] [Options]',
+                '',
+                'Command:',
+                '',
+                '  babel                es6to5',
+                '    <file>             filename',
+                '  server               establish local server',
+                '    <number>           port',
+                '  folder               establish folder',
+                '  cinit                commonjs browserify',
+                '    <file>             filename',
+                '  uglify               compass javascript',
+                '    -b                 beautiful',
+                '  ci                   auto online update',
+                '    <file>             filename',
+                '    <folder>           folder name',
+                '  cdn                  local proxy',
+                '     -l                local',
+                '     -t                text',
+                '     -o                online'
+            ].join('\n') + '\n')
         }
-	}
+    }
 }
-exports._export=f
+exports._export = f
