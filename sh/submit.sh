80b1eeee039ceb7832dd95bf5b79ba07193c253d
xtx1130 <xtx1130@gmail.com>
2016-11-25 15:40:09 +0800

    leju ci's shell file update

--- sh/submit.sh
+++ sh/submit.sh
@@ -4,7 +4,7 @@ message=$2;
 online='/Users/xtx/Desktop/workspace/online/203/cdn.ljimg.com/trunk/';
 offline='/Users/xtx/Desktop/workspace/trunk/';
 # 文件处理
-if [ $message ] && [[ $filepath =~ ".js" ]];
+if [ $message ] && [[ $filepath =~ ".js" ]] || [[ $filepath =~ ".css" ]];
 then
 	var1=${filepath%%/*};
 	echo '['$var1'] update';
@@ -13,22 +13,25 @@ then
 	sudo cp $offline$filepath $online$filepath;
 	echo '\033[33m['$online$filepath'] update \033[0m' ;
 	cd $online$var1;
-	svn status;
-	svn ci -m \'${message}\'
+	svn ci -m \'${message}\';
+	svn log -v -l2;
 	exit 0;
 # 文件夹处理
 elif [ $message ];
 then 
 	tarpath=${filepath##*/};
 	folder=${filepath%/*};
+	echo $tarpath;
+	echo $folder;
+	echo $online$filepath;
 	cd $online$filepath;
 	svn up;
 	#TO DO：加上了-v结果无法把结果流输出给node
 	echo `sudo tar --exclude '*.svn' -C $offline${folder} -cvf - $tarpath | ( cd $online${folder}; tar -xf -)`;
 	echo '\033[33m['$online$filepath'] update \033[0m' ;
 	cd $online$filepath;
-	svn status;
-	svn ci -m \'${message}\'
+	svn ci -m \'${message}\';
+	svn log -v -l2;
 	exit 0;
 # 没有提示消息
 else
