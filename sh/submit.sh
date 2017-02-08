fdb36c14386a9b3c49a5ee13963021e25eca340c
xtx1130 <xtx1130@gmail.com>
2017-02-08 16:08:50 +0800

    add svn add for #leju ci

--- sh/submit.sh
+++ sh/submit.sh
@@ -3,8 +3,25 @@ filepath=$1;
 message=$2;
 offline=$3;
 online=$4;
+info='';
 readonly online;
 readonly offline;
+function strToArr()
+{
+	OLD_IFS=$IFS;
+	IFS=' ';
+	arr=($*);
+	IFS=$OLD_IFS;
+	len=${#arr[@]};
+	for((i=0;i<$len;i++))
+	{
+		if [[ ${arr[i]} == "?" ]];
+		then
+			svn add ${arr[i+1]}
+		fi
+	}
+	return $arr
+}
 # 文件处理
 if [ $message ] && [[ $filepath =~ ".js" ]] || [[ $filepath =~ ".css" ]] || [[ $filepath =~ ".png" ]] || [[ $filepath =~ ".jpg" ]];
 then
@@ -15,7 +32,9 @@ then
 	sudo cp $offline$filepath $online$filepath;
 	echo '\033[33m['$online$filepath'] update \033[0m' ;
 	cd $online$var1;
-	svn ci -m \'${message}\';
+	info=`svn status`;
+	strToArr $info
+	#svn ci -m \'${message}\';
 	svn log $online$filepath -v -l2;
 	exit 0;
 # 文件夹处理
