eb920b1a9177f2d94e69508a2662f61c25a883fb
xtx1130 <xtx1130@gmail.com>
2017-02-14 16:10:39 +0800

    add unset for clean info.json

---
+++ cli/unset.js
@@ -0,0 +1,14 @@
+'use strict';
+class Unset {
+	constructor() {
+		this.option = {
+		}
+		this.description = "unset info.json"
+	}
+	action() {
+		return (ac) => {
+
+		}
+	}
+}
+exports._export = Unset;
