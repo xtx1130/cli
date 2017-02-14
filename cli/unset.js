3514cc521d86e0ff7058dadb758a1e8dc6070182
xtx1130 <xtx1130@gmail.com>
2017-02-14 16:21:00 +0800

    add unset apis

--- cli/unset.js
+++ cli/unset.js
@@ -1,4 +1,6 @@
 'use strict';
+const fs = require('fs');
+const chalk = require('../deps/chalkInfo');
 class Unset {
 	constructor() {
 		this.option = {
@@ -7,7 +9,12 @@ class Unset {
 	}
 	action() {
 		return (ac) => {
-
+			let file = path.join(__dirname, '../config/info.json');
+			fs.writeFile(file,'',{flag:'w'},function(err){//默认的是w用a做了下测试
+				if(err)
+					console.log(chalk.error + err)
+				console.log(chalk.info + 'file has finished reset!')
+			})
 		}
 	}
 }
