fa84beac441f991dcb539473c42af85079383417
xtx1130 <xtx1130@gmail.com>
2017-05-10 15:53:08 +0800

    add file promisfly:construction:

---
+++ deps/promiseReadfile.js
@@ -0,0 +1,23 @@
+'use strict';
+const fs = require('fs');
+const Promise = require('bluebird');
+let PromRead = Promise.method(
+		path => {
+		let bufferData = [];
+		return new Promise((resolve,reject)=>{
+			if (path && fs.existsSync(path)) {
+				let files = fs.createReadStream(path);
+				files.on('data', data => {
+					bufferData.push(data);
+				})
+				files.on('end', () => {
+					resolve(bufferData.join('').toString());
+				})
+				files.on('error',err => {
+					reject(err)
+				})
+			}
+		})
+	}
+)
+module.exports = PromRead;
