f3ecd594844f63b13d677a4389ace3eae20f07ad
xtx1130 <xtx1130@gmail.com>
2017-05-10 17:01:32 +0800

    add emit for core:construction:

--- bin/xtx.js
+++ bin/xtx.js
@@ -20,7 +20,7 @@ Object.defineProperty(global, 'xtx', {
 });
 let xt = new xtx();
 xt.file = './test/message_r.js';
-console.dir(xt.uglify({sourceMap:true}))
+console.log(xt.uglify({sourceMap:true}))
 return ;
 program
 	.version('2.0.0')
--- deps/lejuCore.js
+++ deps/lejuCore.js
@@ -2,35 +2,55 @@
 const uglify = require('uglify-js2');
 const browser = require('browserify');
 const readFile = require('../deps/promiseReadfile');
-class Base{
+const Promise = require('bluebird');
+const emit = require('events').EventEmitter;
+class Base extends emit{
 	constructor(){
+		super()
 		this.file = void 0;
-		this.data = void 0;
+		this.data = {};
+		this.methodPath = [];
+	}
+	_emitUglify(){
+		this.emit('uglifyFinish')
+	}
+	_emitCommon(){
+		this.emit('commonFinish')
 	}
 	uglify(opts={}){//opts.sourceMap='path'
 		let opt = {fromString:true};
 		let fileName = this.file.split('/').pop().split('.')[0];
-		let genera = async ()=>{
-			let da = await readFile(this.file);
-			return da;
-		};
-		genera().then(res=>{
-			this.data = res;
+		let uglifyFunc = res => {
+			this.data.code = res;
 			if(opts.sourceMap)
 				opt.outSourceMap = fileName +'.out.js.map';
 			if(!opts.md5)
 				opt.outFileName = fileName+'.out.js';
-			let result = uglify.minify(this.data,opt);
+			let result = uglify.minify(this.data.code,opt);
 			this.data = {
 				code:result.code,
 				map:result.map,
 				codeFile:opt.outFileName,
-				mapFile:result.file
+				mapFile:opt.outSourceMap
 			};
-			return this;
-		},err=>{
-			console.log(err)
-		})
+			this.methodPath.push('uglify');
+			this._emitUglify()
+			console.log(this)
+		};
+		if(!this.data.code){
+			let genera = async ()=>{
+				let da = await readFile(this.file);
+				return da;
+			};
+			genera().then(res=>{
+				uglifyFunc(res)
+			}).catch(err=>{
+				throw err
+			});
+		}else{
+			uglifyFunc(this.data.code)
+		}
+		return this;
 	}
 	common(){
 
